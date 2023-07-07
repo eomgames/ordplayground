6fecbc0ac3fbd53a83030ff683f67f24602bdff4
Edge <22534232+eomgames@users.noreply.github.com>
2023-07-07 06:43:57 -0700

    fix: updates line endings
    This was causing issues inside the container

--- ordinals/init.sh
+++ ordinals/init.sh
@@ -4,6 +4,7 @@
 touch /root/.bashrc
 echo "alias ord='/ordinals/ord -r --cookie-file /bitcoin/.bitcoin/regtest/.cookie --rpc-url http://bitcoind:8332/ \$@'" >> /root/.bashrc
 source /root/.bashrc
+
 # create a new wallet, new recieving address, and mine 101 blocks to it
 /ordinals/ord -r --cookie-file /bitcoin/.bitcoin/regtest/.cookie --rpc-url http://bitcoind:8332 wallet create
 ADDRESS=$(/ordinals/ord -r --cookie-file /bitcoin/.bitcoin/regtest/.cookie --rpc-url http://bitcoind:8332 wallet receive 2>/dev/null | grep -o -E "\"address\": \"[^\"]+\"" | sed -E "s/\"address\": \"(.*)\"/\1/")
